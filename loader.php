6e485c417563df1a872bbde0513ada8972da0c5e
Jeroen Schmit <jeroen@slimndap.com>
2014-11-18 22:25:59 +0100

    Basic setup for a plugin that extends the Theater for WordPress plugin.

---
+++ loader.php
@@ -0,0 +1,31 @@
+<?php
+/*
+Plugin Name: Related Productions for Theater
+Version: 0.1
+Description: Add related productions to the Theater for WordPress plugin.
+Author: Menno Luitjes
+Author URI: http://mennoluitjes.nl
+Plugin URI: http://mennoluitjes.nl
+Text Domain: wpt_related
+Domain Path: /languages
+*/
+
+if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+
+/**
+ * Loads the WPT_Related class.
+ *
+ * Triggered by the `wpt_loaded` action, which is fired after the Theater for WordPress plugin is loaded.
+ * 
+ * @access public
+ * @return void
+ */
+function wpt_related_loader() {
+	global $wp_theatre;
+	
+	require_once(dirname(__FILE__) . '/wpt_related.php');		
+
+	$wp_theatre->related = new WPT_Related();
+}
+
+add_action('wpt_loaded', 'wpt_related_loader');
---
+++ wpt_related.php
@@ -0,0 +1,19 @@
+<?php
+
+	if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+
+	/**
+	 * WPT_Related class.
+	 *
+	 * Adds related productions to the Theater for WordPress plugin.
+	 */
+	 
+	class WPT_Related {
+		
+		function __construct() {
+
+			// Go Menno!
+
+		}
+		
+	}
